5b32fcb29ca14e2ef620628dd2c2aa6dfdfe17d4
Viennet <viennet.t@gmail.com>
2020-04-17 18:31:56 +0200

    image header

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
 <br>
-<a href="https://www.instagram.com/la_carte_des_chefs/" alt="Profil Instagram" target="_blank"><img src="images/Profil_Instagram.png" alt="Profil Instagram"/></a>
+<a href="https://www.instagram.com/la_carte_des_chefs/" alt="Profil Instagram" target="_blank"><img src="images/instagram.svg" width="30px" alt="Profil Instagram"/></a>
 <p style="padding-bottom:50px;">© Copyright 2020 Tous droits réservés</p>
 </footer>
